778e95b6a7239ffe555ff610e50aeb93b4a1819b
mertkaanerdem <mmertkaanerdem@gmail.com>
2021-07-13 16:50:31 +0300

    adding some styling and updating components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <h1 className="header">
+      Rick <span>And</span> Morty
+    </h1>
+  );
+}
+
+export default Header;
